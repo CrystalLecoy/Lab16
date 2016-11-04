d96b77c2a267b54ea7d6b8e6dfcd15fd13a8dd93
Crystal LeCoy <crystallecoy@gmail.com>
2016-11-04 14:30:13 -0400

    Add project files.

---
+++ ConsoleApplication1_Lab16/Methods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1_Lab16
+{
+    class Methods
+    {
+        public void isAPrime (long LengthOfPrimeNumbersMax)
+        {
+            List<long> PrimeNumbers = new List<long>();
+
+            int i = 0;
+            do
+            {
+                bool isPrime = true;
+                for (long j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    PrimeNumbers.Add(i);
+                }
+                i++;
+            } while (PrimeNumbers.Count < LengthOfPrimeNumbersMax+2);
+            Console.WriteLine($"The number {LengthOfPrimeNumbersMax} prime is: {PrimeNumbers[(int)LengthOfPrimeNumbersMax - 1+2]}");
+
+        }
+    }
+}
---
+++ ConsoleApplication1_Lab16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1_Lab16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's write some primes!");
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("This application will find you any prime, in order, from first primes number on.");
+            Console.WriteLine("Which number are you looking for?");
+            long userInput = Int64.Parse(Console.ReadLine());
+
+            Methods Run = new Methods();
+            Run.isAPrime(userInput);
+
+            //Console.WriteLine("Would you like to continue? Yes or No");
+
+        }
+    }
+}
+
